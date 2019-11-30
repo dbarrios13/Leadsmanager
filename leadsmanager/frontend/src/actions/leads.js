72fb4de5e1230626f04267bda1d4067d0512db97
David Barrios <barriosadavid@gmail.com>
2019-11-30 16:15:03 -0700

    implement knox, and authentication in the backend

--- leadsmanager/frontend/src/actions/leads.js
+++ leadsmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { createMessage } from "./messages";
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+import { createMessage, returnErrors } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // Get leads
 export const getLeads = () => dispatch => {
@@ -10,7 +10,7 @@ export const getLeads = () => dispatch => {
       dispatch({ type: GET_LEADS, payload: res.data });
     })
     .catch(err => {
-      console.log(err);
+      dispatch(returnErrors(err.response.data, err.response.status));
     });
 };
 
@@ -22,13 +22,9 @@ export const addLead = lead => dispatch => {
       dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({ type: ADD_LEAD, payload: res.data });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-      dispatch({ type: GET_ERRORS, payload: errors });
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 // delete lead
