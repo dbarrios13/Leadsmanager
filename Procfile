5c17b91ca34fd444215517b646c0e809092bca8c
dbarrios13 <barriosadavid@gmail.com>
2019-12-03 19:25:57 -0700

    added config settings for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn leadsmanager.wsgi --log-file -
--- leadsmanager/leadsmanager/settings.py
+++ leadsmanager/leadsmanager/settings.py
@@ -52,6 +52,8 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
